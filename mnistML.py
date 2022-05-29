b676c348c3ddebb693e6197e9b1022a495423030
Lyle <carlylemorris@gmail.com>
2022-05-29 12:44:03 -0500

    Uploaded project from early mid 2020. Requires Theano 1.0.4.

---
+++ mnistML.py
@@ -0,0 +1,19 @@
+import numpy as np,theano,mnist_loader,NetworkLib,numpy as np,time
+
+tdata, vdata, test = mnist_loader.load_data_wrapper() #Stole from Neural Networks and Deep Learning book
+
+network = NetworkLib.Network([784,200,200,10])
+
+for epoch in range(10):
+    c = 0
+    for x,y in vdata:
+        if np.argmax(network.feedForward(x)) == np.argmax(y):
+            c = c + 1
+    print("\nEpoch",epoch,"\nTrain Cost:",network.evaluate(tdata),"\nValidation Cost:",network.evaluate(vdata),"\nValidation % Accuracy:",c*100/len(vdata))
+    network.strain(tdata,1,.001)
+
+c = 0
+for x,y in vdata:
+    if np.argmax(network.feedForward(x)) == np.argmax(y):
+        c = c + 1
+print("Final test % accuracy:",c*100/len(test))    
